61e2714a057d6a76eb69e6dff324430ff82b9ed2
Vyshak-NSC <vyshaknsc02@gmail.com>
2025-07-02 07:11:34 +0530

    config update

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,17 @@ from flask import Flask, jsonify
 from dotenv import load_dotenv
 from app.extensions import db, migrate, ma, socketio
 from flask_cors import CORS
-from .config import Config
+# from .config import Config
+import os
 
 load_dotenv()
 from app.models import *
-
+class Config:
+    FLASK_APP = 'app'
+    SECRET_KEY = os.getenv('SECRET_KEY','FALLBACK_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL','sqlite:///todo.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
