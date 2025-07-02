31ee5c3a6a8634fcc02f6a0e7ac4174f86cfb277
Vyshak-NSC <vyshaknsc02@gmail.com>
2025-07-02 20:17:32 +0530

    savepoint

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ from app.models import *
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config.from_object('config.Config')
     
     db.init_app(app)
     ma.init_app(app)
